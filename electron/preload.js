86d011c8e1484334dde2dcee1c9fd141c19e5b41
Kashi828 <217323527+Kashi828@users.noreply.github.com>
2026-09-10 19:34:46 +0530

    fix: expose secure desktop auth context

--- electron/preload.js
+++ electron/preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld("zyraDesktopBridge", {
   ensureVoiceSession: () => ipcRenderer.invoke("zyra:voice-session"),
   getVoiceAuthHeaders: () => ipcRenderer.invoke("zyra:voice-auth"),
   logoutVoiceSession: () => ipcRenderer.invoke("zyra:voice-logout"),
+  getAuthContext: () => ipcRenderer.invoke("zyra:auth-context"),
   ping: () => ipcRenderer.invoke("zyra:ping"),
 });
